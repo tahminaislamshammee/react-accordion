75216557057003f7f9424074e1ecbf51fd658a61
tahminaislamshammee <tahminaislamshammee@gmail.com>
2022-10-08 20:43:12 +0600

    accordion component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import CommonAccordion from './Components/CommonAccordion';
+
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts?_start=0&_limit=5')
+  .then(response => response.json())
+  .then(items => setData(items))
+  },[])
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>accordion</h1>
+      <CommonAccordion data={data}/>
     </div>
+    </>
   );
 }
 
---
+++ src/Pages/Page2.jsx
@@ -0,0 +1,7 @@
+const Page2 = () => {
+    return ( 
+        <h2>page 2</h2>
+    );
+}
+ 
+export default Page2;
